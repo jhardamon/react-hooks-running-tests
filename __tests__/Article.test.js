80b6d1959431c5b7a389fc4f42cb19bb0ef9ca17
Joel <jchardamon@gmail.com>
2023-08-08 23:00:10 -0400

    Completion of code-along

---
+++ __tests__/Article.test.js
@@ -0,0 +1,16 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import Article from "../components/Article";
+
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  const element = screen.queryByText("please pass this test");
+
+  screen.debug();
+
+
+  expect(element).toBeInTheDocument();
+});
---
+++ __tests__/Header.test.js
@@ -0,0 +1,13 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import Header from "../components/Header";
+
+test("displays the text 'hello from the Header!'", () => {
+  render(<Header />);
+
+  screen.debug();
+  
+  expect(screen.queryByText("hello from the Header!")).toBeInTheDocument();
+});
